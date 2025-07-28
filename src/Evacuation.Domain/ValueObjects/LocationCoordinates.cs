c96ca7321238b264d7f47dc2dbff4c79a1cba476
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 10:57:26 +0700

    Refactor : change set location entity

--- src/Evacuation.Domain/ValueObjects/LocationCoordinates.cs
+++ src/Evacuation.Domain/ValueObjects/LocationCoordinates.cs
@@ -2,17 +2,7 @@
 {
     public class LocationCoordinates
     {
-        public double Latitude { get; }
-        public double Longitude { get; }
-        public LocationCoordinates(double latitude, double longitude)
-        {
-            if (latitude < -90 || latitude > 90)
-                throw new ArgumentOutOfRangeException(nameof(latitude), "Latitude must be between -90 and 90 degrees.");
-            if (longitude < -180 || longitude > 180)
-                throw new ArgumentOutOfRangeException(nameof(longitude), "Longitude must be between -180 and 180 degrees.");
-            Latitude = latitude;
-            Longitude = longitude;
-        }
-        public override string ToString() => $"{Latitude}, {Longitude}";
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
