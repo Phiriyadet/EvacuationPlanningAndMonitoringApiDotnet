f4075174695854bd94862ed7b9c8208796b2b585
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 12:38:13 +0700

    Add : Plan Entity

---
+++ src/Evacuation.Domain/Entities/EvacuationPlan.cs
@@ -0,0 +1,12 @@
+﻿namespace Evacuation.Domain.Entities
+{
+    public class EvacuationPlan
+    {
+        public long PlanId { get; set; }
+        public string ZoneId { get; set; } = string.Empty;
+        public string VehicleId { get; set; } = string.Empty;
+        public int NumberOfEvacuatedPeople { get; set; }
+        public string ETA { get; set; } = string.Empty; // Estimated Time of Arrival
+
+    }
+}
