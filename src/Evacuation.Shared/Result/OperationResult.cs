088269ba94baf2b057f8bd61b233842e82cc83b6
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 17:08:49 +0700

    Add : OperationResult

---
+++ src/Evacuation.Shared/Result/OperationResult.cs
@@ -0,0 +1,35 @@
+﻿namespace Evacuation.Shared.Result
+{
+    public class OperationResult<T>
+    {
+        public bool Success { get; private set; }
+        public string? Message { get; private set; }
+        public T? Data { get; private set; }
+        public Exception? Exception { get; private set; }
+        public Dictionary<string, string[]>? Errors { get; private set; }
+
+        private OperationResult(bool success, string? message = null, T? data = default, Exception? exception = null, Dictionary<string, string[]>? errors = null)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+            Exception = exception;
+            Errors = errors;
+        }
+
+        public static OperationResult<T> Ok(T data, string? message = null) 
+            => new OperationResult<T>(true, message, data);
+        
+        public static OperationResult<T> Ok(string? message = null) 
+            => new OperationResult<T>(true, message);
+
+        public static OperationResult<T> Fail(string message, T? data, Exception? exception = null)
+            => new OperationResult<T>(false, message, data, exception);
+
+        public static OperationResult<T> Fail(Dictionary<string, string[]> errors, string? message = null)
+            => new OperationResult<T>(false, message, default, null, errors);
+
+    }
+
+
+}
