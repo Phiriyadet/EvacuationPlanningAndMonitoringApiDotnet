2af7fae660d45a39197b179deb5938c2fa0a23d4
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 11:28:53 +0700

    Add : enum VehicleType

---
+++ src/Evacuation.Domain/Enums/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evacuation.Domain.Enums
+{
+    public enum VehicleType
+    {
+        Unknown = 0,
+        Bus = 1,
+        Ambulance = 2,
+        Truck = 3,
+        Boat = 4,
+        Helicopter = 5,
+        Van = 6,
+        Other = 99
+    }
+
+}
