80d0e7350b522d9397477e428321b8a2071eda78
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 12:38:37 +0700

    Add : Status Entity

---
+++ src/Evacuation.Domain/Entities/EvacuationStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Evacuation.Domain.Entities
+{
+    public class EvacuationStatus
+    {
+        public string ZoneId { get; set; } = string.Empty;
+
+        public int TotalPeopleEvacuated { get; set; }
+        public int RemainingPeople { get; set; }
+
+        public string? LastVehicleId { get; set; }
+
+    }
+}
