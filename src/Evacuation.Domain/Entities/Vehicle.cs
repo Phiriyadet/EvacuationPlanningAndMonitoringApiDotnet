455e1cf458a3bc5aaf11c6b928cfa2933b098b9f
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 11:55:58 +0700

    Add : Vehicle Entity

---
+++ src/Evacuation.Domain/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using Evacuation.Domain.Enums;
+using Evacuation.Domain.ValueObjects;
+
+namespace Evacuation.Domain.Entities
+{
+    public class Vehicle
+    {
+        public string VehicleId { get; set; } = string.Empty;
+        public VehicleType Type { get; set; } = VehicleType.Unknown;
+        public int Capacity { get; set; }
+        public int Speed { get; set; } // Speed in km/h
+        public LocationCoordinates LocationCoordinates { get; set; } = new LocationCoordinates();
+    }
+}
