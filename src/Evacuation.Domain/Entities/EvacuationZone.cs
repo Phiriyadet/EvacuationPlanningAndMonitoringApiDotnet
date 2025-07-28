aee3670de434b438936204f1361420c334686406
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 12:39:47 +0700

    Refector : Zone Entity

--- src/Evacuation.Domain/Entities/EvacuationZone.cs
+++ src/Evacuation.Domain/Entities/EvacuationZone.cs
@@ -5,8 +5,13 @@ namespace Evacuation.Domain.Entities
     public class EvacuationZone
     {
         public string ZoneId { get; set; } = string.Empty;
+
         public int NumberOfPeople { get; set; }
         public int UrgencyLevel { get; set; }
+
         public LocationCoordinates LocationCoordinates { get; set; } = new LocationCoordinates();
+
+        private EvacuationZone() { } // cannot be instantiated directly
+
     }
 }
