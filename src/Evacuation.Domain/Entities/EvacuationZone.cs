03f89ac2b0797bc77405e7e96ce1476ef4b86208
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-28 11:24:53 +0700

    Add : EvacuationZone Entity

---
+++ src/Evacuation.Domain/Entities/EvacuationZone.cs
@@ -0,0 +1,19 @@
+﻿using Evacuation.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evacuation.Domain.Entities
+{
+    public class EvacuationZone
+    {
+        public string ZoneId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int NumberOfPeople { get; set; }
+        public int UrgencyLevel { get; set; }
+        public LocationCoordinates LocationCoordinates { get; set; } = new LocationCoordinates();
+    }
+}
