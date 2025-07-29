3e2a1286f1c1426296365aa51c9e8383d6659181
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 15:43:54 +0700

    Add : Dto CreateStatusDto

---
+++ src/Evacuation.Application/DTOs/Status/CreateStatusDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Evacuation.Application.DTOs.Status
+{
+    public class CreateStatusDto
+    {
+        public string ZoneId { get; set; } = string.Empty;
+
+        public int TotalPeopleEvacuated { get; set; }
+        public int RemainingPeople { get; set; }
+
+        public string? LastVehicleId { get; set; }
+    }
+}
