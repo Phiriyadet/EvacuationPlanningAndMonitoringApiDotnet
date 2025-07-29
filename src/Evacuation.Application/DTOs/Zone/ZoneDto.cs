5e0fd4c536dd2edea04bda89b0c9e533b3db8e5c
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 12:15:27 +0700

    Add : Dto ZoneDto

---
+++ src/Evacuation.Application/DTOs/Zone/ZoneDto.cs
@@ -0,0 +1,12 @@
+﻿using Evacuation.Application.DTOs.Location;
+
+namespace Evacuation.Application.DTOs.Zone
+{
+    public class ZoneDto
+    {
+        public string ZoneId { get; set; } = string.Empty;
+        public int NumberOfPeople { get; set; }
+        public int UrgencyLevel { get; set; }
+        public LocationCoordinatesDto Location { get; set; } = new LocationCoordinatesDto();
+    }
+}
