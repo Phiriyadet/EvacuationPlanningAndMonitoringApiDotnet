13b0f1988b17ca2b98192dd5425a5e6fb950dd55
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 15:48:10 +0700

    Add : Dto UpdateStatusDto

---
+++ src/Evacuation.Application/DTOs/Status/UpdateStatusDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Evacuation.Application.DTOs.Status
+{
+    public class UpdateStatusDto
+    {
+        public int TotalPeopleEvacuated { get; set; }
+        public int RemainingPeople { get; set; }
+
+        public string? LastVehicleId { get; set; }
+    }
+}
