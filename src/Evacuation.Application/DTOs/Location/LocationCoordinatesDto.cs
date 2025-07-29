8a2aab432d12a34b2bbdd8d678b620e08e838235
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 11:21:35 +0700

    Add : Dto LocationCoordinatesDto

---
+++ src/Evacuation.Application/DTOs/Location/LocationCoordinatesDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Evacuation.Application.DTOs.Location
+{
+    public class LocationCoordinatesDto
+    {
+        [Range(-90, 90, ErrorMessage = "Latitude must between -90 and 90 degrees.")]
+        public double Latitude { get; set; }
+
+        [Range(-180, 180, ErrorMessage = "Longitude must be between -180 and 180 degrees.")]
+        public double Longitude { get; set; }
+    }
+}
