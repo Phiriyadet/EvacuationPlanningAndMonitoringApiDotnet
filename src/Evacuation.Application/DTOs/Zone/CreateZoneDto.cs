2b0d8a9c6f53f33051b47313d1cc05b134725d80
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 11:36:28 +0700

    Add : Dto CreateZoneDto

---
+++ src/Evacuation.Application/DTOs/Zone/CreateZoneDto.cs
@@ -0,0 +1,19 @@
+﻿using Evacuation.Application.DTOs.Location;
+using System.ComponentModel.DataAnnotations;
+
+namespace Evacuation.Application.DTOs.Zone
+{
+    public class CreateZoneDto
+    {
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Number of people must be a non-negative integer.")]
+        public int NumberOfPeople { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Urgency level must be between 1 and 5.")]
+        public int UrgencyLevel { get; set; }
+
+        [Required]
+        public LocationCoordinatesDto Location { get; set; } = new LocationCoordinatesDto();
+    }
+}
