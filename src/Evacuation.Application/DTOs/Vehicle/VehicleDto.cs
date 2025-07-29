bc6736b9952d93d9cba1504d565cf44e03be58ec
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 12:43:05 +0700

    Add : Dto VehicleDto

---
+++ src/Evacuation.Application/DTOs/Vehicle/VehicleDto.cs
@@ -0,0 +1,17 @@
+﻿using Evacuation.Application.DTOs.Location;
+using Evacuation.Domain.Enums;
+
+namespace Evacuation.Application.DTOs.Vehicle
+{
+    public class VehicleDto
+    {
+        public string VehicleId { get; set; } = string.Empty;
+
+        public VehicleType Type { get; set; } = VehicleType.Unknown;
+        public int Capacity { get; set; }
+        public int Speed { get; set; } // Speed in km/h
+        public bool IsAvailable { get; set; } = true; // Indicates if the vehicle is available for use
+
+        public LocationCoordinatesDto Location { get; set; } = new LocationCoordinatesDto();
+    }
+}
