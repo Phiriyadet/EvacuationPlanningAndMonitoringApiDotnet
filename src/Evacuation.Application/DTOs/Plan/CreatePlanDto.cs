1f70b1757bd28022f383705b766869c97124129b
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 15:18:35 +0700

    Add : Dto CreatePlanDto

---
+++ src/Evacuation.Application/DTOs/Plan/CreatePlanDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Evacuation.Application.DTOs.Plan
+{
+    public class CreatePlanDto
+    {
+        public string ZoneId { get; set; } = string.Empty;
+        public string VehicleId { get; set; } = string.Empty;
+        public int NumberOfEvacuatedPeople { get; set; }
+        public string ETA { get; set; } = string.Empty; // Estimated Time of Arrival
+    }
+}
