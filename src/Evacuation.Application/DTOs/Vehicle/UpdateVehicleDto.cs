9c32173508b9c7925d3212a0ce9244809ca1e891
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-29 13:24:22 +0700

    Add : Dto UpdateVehicleDto

---
+++ src/Evacuation.Application/DTOs/Vehicle/UpdateVehicleDto.cs
@@ -0,0 +1,21 @@
+﻿using Evacuation.Application.DTOs.Location;
+using Evacuation.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Evacuation.Application.DTOs.Vehicle
+{
+    public class UpdateVehicleDto
+    {
+        
+        [EnumDataType(typeof(VehicleType), ErrorMessage = "Invalid vehicle type.")]
+        public VehicleType Type { get; set; } = VehicleType.Unknown;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Capacity must be a non-negative integer.")]
+        public int Capacity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Speed must be a non-negative integer.")]
+        public int Speed { get; set; } // Speed in km/h
+
+        public LocationCoordinatesDto location { get; set; } = new LocationCoordinatesDto();
+    }
+}
