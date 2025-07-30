f4f32facdf47842730418d105787fa9200fea992
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-30 14:45:10 +0700

    Add : Interface IEvacuationService

---
+++ src/Evacuation.Application/Interfaces/IServices/IEvacuationService.cs
@@ -0,0 +1,19 @@
+﻿using Evacuation.Application.DTOs.Plan;
+using Evacuation.Application.DTOs.Status;
+using Evacuation.Shared.Result;
+
+namespace Evacuation.Application.Interfaces.IServices
+{
+    public interface IEvacuationService
+    {
+        Task<OperationResult<IEnumerable<StatusDto>>> GetAllStatusAsync();
+        Task<OperationResult<StatusDto>> GetStatusByIdAsync(string statusId);
+        Task<OperationResult<StatusDto>> UpdateStatusByPlanAsync();
+        Task<OperationResult<PlanDto>> CreatePlanAsync(double distanceKm);
+        Task<OperationResult<PlanDto>> GetPlanByZoneIdAsync(string zoneId);
+        Task<OperationResult<IEnumerable<PlanDto>>> GetAllPlansAsync();
+        Task<OperationResult<bool>> ClearAllPlanAndStatusAsync();
+
+
+    }
+}
