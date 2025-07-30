38345e33218c4963d88888b76184197bed20524e
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-30 13:49:08 +0700

    Add : Interface IVehicleService

---
+++ src/Evacuation.Application/Interfaces/IServices/IVehicleService.cs
@@ -0,0 +1,14 @@
+﻿using Evacuation.Application.DTOs.Vehicle;
+using Evacuation.Shared.Result;
+
+namespace Evacuation.Application.Interfaces.IServices
+{
+    public interface IVehicleService
+    {
+        Task<OperationResult<IEnumerable<VehicleDto>>> GetAllVehiclesAsync();
+        Task<OperationResult<VehicleDto>> GetVehicleByIdAsync(string vehicleId);
+        Task<OperationResult<VehicleDto>> UpdateVehicleAsync(UpdateVehicleDto updateDto);
+        Task<OperationResult<bool>> DeleteVehicleAsync(string vehicleId);
+        Task<OperationResult<VehicleDto>> AddVehicleAsync(CreateVehicleDto createDto);
+    }
+}
