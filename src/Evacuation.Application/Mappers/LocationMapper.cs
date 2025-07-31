d31e7219971d607410092786730c055b0b2cfd00
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-07-31 15:40:46 +0700

    Add : Mapper LocationMapper

---
+++ src/Evacuation.Application/Mappers/LocationMapper.cs
@@ -0,0 +1,27 @@
+﻿using Evacuation.Application.DTOs.Location;
+using Evacuation.Domain.ValueObjects;
+
+namespace Evacuation.Application.Mappers
+{
+    public static class LocationMapper
+    {
+        public static LocationCoordinatesDto ToDto(this LocationCoordinates location)
+        {
+            return new LocationCoordinatesDto
+            {
+                Latitude = location.Latitude,
+                Longitude = location.Longitude,
+            };
+        }
+        
+        public static LocationCoordinates ToEntity(this LocationCoordinatesDto locationDto)
+        {
+            return new LocationCoordinates
+            {
+                Latitude = locationDto.Latitude,
+                Longitude = locationDto.Longitude,
+            };
+        }
+    }
+      
+}
