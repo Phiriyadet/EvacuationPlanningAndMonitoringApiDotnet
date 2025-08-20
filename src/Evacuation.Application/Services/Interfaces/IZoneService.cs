68a5fbcf34771ae541367af06001fcaeeaa1bc6a
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-20 13:55:35 +0700

    Add : Interface IZoneService

--- src/Evacuation.Application/Services/Interfaces/IZoneService.cs
+++ src/Evacuation.Application/Services/Interfaces/IZoneService.cs
@@ -6,9 +6,9 @@ namespace Evacuation.Application.Services.Interfaces
     public interface IZoneService
     {
         Task<OperationResult<IEnumerable<ZoneDto>>> GetAllZonesAsync();
-        Task<OperationResult<ZoneDto>> GetZoneByIdAsync(string zoneId);
-        Task<OperationResult<ZoneDto>> UpdateZoneAsync(string Id, UpdateZoneDto updateDto);
-        Task<OperationResult<bool>> DeleteZoneAsync(string zoneId);
+        Task<OperationResult<ZoneDto>> GetZoneByIdAsync(int zoneId);
+        Task<OperationResult<ZoneDto>> UpdateZoneAsync(int Id, UpdateZoneDto updateDto);
+        Task<OperationResult<bool>> DeleteZoneAsync(int zoneId);
         Task<OperationResult<ZoneDto>> AddZoneAsync(CreateZoneDto createDto);
     }
 }
