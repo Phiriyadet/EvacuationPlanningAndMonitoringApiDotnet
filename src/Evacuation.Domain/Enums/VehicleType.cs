644ee9fd3c9602e51cffc967f37485cf7c817ef3
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-20 15:12:46 +0700

    Remove : remove using is that not use

--- src/Evacuation.Domain/Enums/VehicleType.cs
+++ src/Evacuation.Domain/Enums/VehicleType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Evacuation.Domain.Enums
+﻿namespace Evacuation.Domain.Enums
 {
     public enum VehicleType
     {
