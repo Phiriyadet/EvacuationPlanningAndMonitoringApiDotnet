dd5acb28663728be4a76155a66611cd58b3940fc
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-21 14:30:15 +0700

    Add : add EvacuationController

---
+++ src/Evacuation.API/Controllers/EvacuationController.cs
@@ -0,0 +1,152 @@
+﻿using Evacuation.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Evacuation.API.Controllers
+{
+    [Route("api/evacuations")]
+    [ApiController]
+    public class EvacuationController : ControllerBase
+    {
+        private readonly IEvacuationService _evacuationService;
+        public EvacuationController(IEvacuationService evacuationService)
+        {
+            _evacuationService = evacuationService;
+        }
+
+        // GET: api/evacuation/plan
+        [HttpGet("plan")]
+        public async Task<IActionResult> GetAllPlans()
+        {
+            var result = await _evacuationService.GetAllPlansAsync();
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving evacuation plans", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // GET: api/evacuation/status
+        [HttpGet("status")]
+        public async Task<IActionResult> GetAllStatuses()
+        {
+            var result = await _evacuationService.GetAllStatusAsync();
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving evacuation statuses", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // GET api/<EvacuationController>/plan/5
+        [HttpGet("plan/zoneId/{id}")]
+        public async Task<IActionResult> GetPlanByIdAsync(int id)
+        {
+            var result = await _evacuationService.GetPlanByZoneIdAsync(id);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving the evacuation plan", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // GET api/<EvacuationController>/status/5
+        [HttpGet("status/{id}")]
+        public async Task<IActionResult> GetStatusByIdAsync(int id)
+        {
+            var result = await _evacuationService.GetStatusByIdAsync(id);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving the evacuation status", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // POST api/<EvacuationController>
+        [HttpPost("plan")]
+        public async Task<IActionResult> PostPlan(double distanceKm=100)
+        {
+            var result = await _evacuationService.CreatePlanAsync(distanceKm);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while creating the evacuation plan", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // PUT api/<EvacuationController>/5
+        [HttpPut("update")]
+        public async Task<IActionResult> UpdateStatus()
+        {
+            var result = await _evacuationService.UpdateStatusByPlanAsync();
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while updating the evacuation status", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // DELETE api/<EvacuationController>/5
+        [HttpDelete("clear")]
+        public async Task<IActionResult> ClearEvacuations()
+        {
+            var result = await _evacuationService.ClearAllPlanAndStatusAsync();
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while clearing evacuations", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.Message });
+                }
+            }
+            return Ok(new { Message = "All evacuation plans and statuses cleared successfully" });
+
+        }
+    }
+}
