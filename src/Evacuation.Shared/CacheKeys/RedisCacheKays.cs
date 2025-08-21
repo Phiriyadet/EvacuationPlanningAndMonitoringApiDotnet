6c20d17256e496c4e454935d60eab3b52ee6997f
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-21 14:33:57 +0700

    Add : RedisCacheKeys

---
+++ src/Evacuation.Shared/CacheKeys/RedisCacheKays.cs
@@ -0,0 +1,13 @@
+﻿namespace Evacuation.Shared.CacheKeys
+{
+    public static class RedisCacheKays
+    {
+        public const string VehicleCacheKey = "VehicleCacheKey";
+        public const string ZoneCacheKey = "ZoneCacheKey";
+        public const string PlanCacheKey = "PlanCacheKey";
+        public const string StatusCacheKey = "StatusCacheKey";
+
+        public const string PlanHashKey = "PlanHashKey";
+        public const string StatusHashKey = "StatusHashKey";
+    }
+}
