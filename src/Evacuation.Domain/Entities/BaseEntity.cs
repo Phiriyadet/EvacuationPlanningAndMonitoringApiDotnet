7f4c03037c4789406c336ef87e60009c03dea82d
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-21 21:50:23 +0700

    Add and Refactor : add BaseEntity and refactor BaseEntityWithPrefix

---
+++ src/Evacuation.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Evacuation.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; protected set; }  // PK, EF Core ใช้ได้
+        public DateTimeOffset CreatedAt { get; protected set; }
+        public DateTimeOffset UpdatedAt { get; protected set; }
+
+        protected BaseEntity()
+        {
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        // อัปเดตเวลาแก้ไข
+        public void SetUpdateAt() => UpdatedAt = DateTimeOffset.UtcNow;
+    }
+}
--- src/Evacuation.Domain/Entities/BaseEntityWithPrefix.cs
+++ src/Evacuation.Domain/Entities/BaseEntityWithPrefix.cs
@@ -1,25 +1,11 @@
 ﻿namespace Evacuation.Domain.Entities
 {
-    public abstract class BaseEntityWithPrefix
+    public abstract class BaseEntityWithPrefix : BaseEntity
     {
-        public int Id { get; protected set; }  // PK, EF Core ใช้ได้
-
         public abstract string Prefix { get; } // กำหนด prefix ใน entity ลูก
 
         // BusinessId สร้างจาก prefix + Id เช่น Z-5, V-10
         public string BusinessId => $"{Prefix}-{Id}";
-
-        public DateTimeOffset CreatedAt { get; protected set; }
-        public DateTimeOffset UpdatedAt { get; protected set; }
-
-        protected BaseEntityWithPrefix()
-        {
-            CreatedAt = DateTimeOffset.UtcNow;
-            UpdatedAt = DateTimeOffset.UtcNow;
-        }
-
-        // อัปเดตเวลาแก้ไข
-        public void SetUpdateAt() => UpdatedAt = DateTimeOffset.UtcNow;
     }
 
 
