63e59c19d2404bb89de435fb28f0d6069a21b870
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-22 14:33:57 +0700

    Add : new IGenericWithPrefixRepo and GenericWithPrefixRepo

---
+++ src/Evacuation.Infrastructure/Repositories/GenericWithPrefixRepository.cs
@@ -0,0 +1,23 @@
+﻿using Evacuation.Domain.Entities;
+using Evacuation.Infrastructure.Data.AppDbContext;
+using Evacuation.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evacuation.Infrastructure.Repositories
+{
+    public class GenericWithPrefixRepository<T, TKey>
+            : GenericRepository<T, TKey>, IGenericWithPrefixRepository<T, TKey>
+            where T : BaseEntityWithPrefix
+            where TKey : notnull
+    {
+        public GenericWithPrefixRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Task<Dictionary<TKey, string>> GetIdMapAsync()
+        {
+            return _dbSet.AsNoTracking()
+            .ToDictionaryAsync(e => (TKey)(object)e.Id, e => e.BusinessId);
+        }
+    }
+}
---
+++ src/Evacuation.Infrastructure/Repositories/Interfaces/IGenericWithPrefixRepository.cs
@@ -0,0 +1,11 @@
+﻿using Evacuation.Domain.Entities;
+
+namespace Evacuation.Infrastructure.Repositories.Interfaces
+{
+    public interface IGenericWithPrefixRepository<T, TKey> : IGenericRepository<T, TKey>
+            where T : BaseEntityWithPrefix
+            where TKey : notnull
+    {
+        Task<Dictionary<TKey, string>> GetIdMapAsync();
+    }
+}
