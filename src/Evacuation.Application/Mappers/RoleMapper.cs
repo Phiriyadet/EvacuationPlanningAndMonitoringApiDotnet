7d7877985d001fb26b7e22a3769a3888baa91616
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-22 17:26:05 +0700

    Add : new Mapper of User and Role

---
+++ src/Evacuation.Application/Mappers/RoleMapper.cs
@@ -0,0 +1,34 @@
+﻿using Evacuation.Application.DTOs.Role;
+using Evacuation.Domain.Entities;
+
+
+namespace Evacuation.Application.Mappers
+{
+    public static class RoleMapper
+    {
+        public static RoleDto ToDto(this Role role)
+        {
+            return new RoleDto
+            {
+                Id = role.Id,
+                Name = role.Name,
+                Description = role.Description,
+            };
+        }
+
+        public static Role CreateToEntity(this CreateRoleDto createDto)
+        {
+            return new Role(createDto.Name,createDto.Description);
+        }
+
+        public static Role UpdateToEntity(this UpdateRoleDto updateDto, Role existingRole)
+        {
+            // Use null-coalescing operator to provide default values if properties are null
+            existingRole.Update(
+                updateDto.Name ?? string.Empty,
+                updateDto.Description ?? string.Empty
+            );
+            return existingRole;
+        }
+    }
+}
---
+++ src/Evacuation.Application/Mappers/UserMapper.cs
@@ -0,0 +1,42 @@
+﻿using Evacuation.Application.DTOs.User;
+using Evacuation.Domain.Entities;
+
+namespace Evacuation.Application.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserDto ToDto(this User user, string roleName)
+        {
+            return new UserDto
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                IsActice = user.IsActive,
+                RoleName = roleName
+            };
+        }
+
+        public static User CreateToEntity(this CreateUserDto createDto, string passwordHash)
+        {
+            return new User
+            (
+                createDto.Username,
+                createDto.Email,
+                passwordHash,
+                createDto.RoleId
+            );
+        }
+
+        public static User UpdateToEntity(this UpdateUserDto updateDto, User existingUser)
+        {
+            existingUser.Update
+            (
+                updateDto.Username,
+                updateDto.Email,
+                updateDto.RoleId
+            );
+            return existingUser;
+        }
+    }
+}
