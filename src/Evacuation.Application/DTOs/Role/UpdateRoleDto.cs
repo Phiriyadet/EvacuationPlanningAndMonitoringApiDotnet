ba6fe4fb1fe386c698afdcbe504aadbb6452446e
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-22 17:25:29 +0700

    Add : new Role Dtos

---
+++ src/Evacuation.Application/DTOs/Role/UpdateRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Evacuation.Application.DTOs.Role
+{
+    public class UpdateRoleDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
