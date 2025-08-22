1b4999b22efd7b1c6c8ec30d95b00b92cb35e88e
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-22 17:16:48 +0700

    Add : new interface and repo of User and Role

---
+++ src/Evacuation.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Evacuation.Domain.Entities;
+using Evacuation.Infrastructure.Data.AppDbContext;
+using Evacuation.Infrastructure.Repositories.Interfaces;
+
+namespace Evacuation.Infrastructure.Repositories
+{
+    public class UserRepository : GenericRepository<User, int>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
