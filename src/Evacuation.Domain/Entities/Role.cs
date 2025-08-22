c0cd5fa0cb6430da1861877858936393a041a71d
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-22 17:24:59 +0700

    Add : new Entities User and Role

---
+++ src/Evacuation.Domain/Entities/Role.cs
@@ -0,0 +1,25 @@
+﻿namespace Evacuation.Domain.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; private set; } = null!;
+        public string Description { get; private set; } = string.Empty;
+
+        protected Role() { }
+
+        public Role(string name, string description = "")
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Role name cannot be empty", nameof(name));
+            Name = name;
+            Description = description;
+        }
+
+        public void Update(string name, string description = "")
+        {
+            if (!string.IsNullOrWhiteSpace(name)) Name = name;
+            Description = description;
+            SetUpdateAt();
+        }
+    }
+}
---
+++ src/Evacuation.Domain/Entities/User.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Evacuation.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; private set; } = null!;
+        public string Email { get; private set; } = null!;
+        public string PasswordHash { get; private set; } = null!; // เก็บ hash
+        public bool IsActive { get; private set; } = true;
+
+        // FK
+        public int RoleId { get; private set; }
+
+        protected User() { }
+
+        public User(string username, string email, string passwordHash, int roleId)
+        {
+            ValidateUser(username, email, passwordHash, roleId);
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            RoleId = roleId;
+        }
+
+        public void Update(string? username = null, string? email = null, int? roleId = null)
+        {
+            if (!string.IsNullOrWhiteSpace(username)) Username = username;
+            if (!string.IsNullOrWhiteSpace(email)) Email = email;
+            if (roleId.HasValue && roleId.Value > 0) RoleId = roleId.Value;
+            SetUpdateAt();
+        }
+
+        public void SetActive(bool isActive)
+        {
+            IsActive = isActive;
+            SetUpdateAt();
+        }
+
+        public void ChangePassword(string newPasswordHash)
+        {
+            if (string.IsNullOrWhiteSpace(newPasswordHash)) throw new ArgumentException("Password is required.");
+            PasswordHash = newPasswordHash;
+            SetUpdateAt();
+        }
+
+
+        private void ValidateUser(string username, string email, string passwordHash, int roleId)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Username is required.", nameof(username));
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email is required.", nameof(email));
+            if (string.IsNullOrWhiteSpace(passwordHash))
+                throw new ArgumentException("PasswordHash is required.", nameof(passwordHash));
+            if (roleId <= 0)
+                throw new ArgumentException("RoleId must be greater than zero.", nameof(roleId));
+        }
+    }
+}
