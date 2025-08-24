d9098bdf30aad0b47fd2bd3a8de8461add2eead6
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-08-24 21:40:55 +0700

    Remove : interface and repo Rolerepo

--- src/Evacuation.Infrastructure/Repositories/Interfaces/IRoleRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Evacuation.Domain.Entities;
-
-namespace Evacuation.Infrastructure.Repositories.Interfaces
-{
-    public interface IRoleRepository : IGenericRepository<Role, int>
-    {
-    }
-}
