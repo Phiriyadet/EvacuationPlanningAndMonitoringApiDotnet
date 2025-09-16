45dd2a0b9c6415eee8735b05464340f74e25c6fd
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-09-16 13:29:11 +0700

    Add : Prefix.cs

---
+++ src/Evacuation.Shared/Prefix/Prefixs.cs
@@ -0,0 +1,9 @@
+﻿namespace Evacuation.Shared.Prefix
+{
+    public static class Prefixs
+    {
+        public const string PlanIdPrefix = "P";
+        public const string VehicleIdPrefix = "V";
+        public const string ZoneIdPrefix = "Z";
+    }
+}
