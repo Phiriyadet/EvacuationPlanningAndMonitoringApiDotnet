227890af438f271569c6f1d64ad95f6c762de54b
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-09-19 14:16:33 +0700

    Refactor and Add : rename interface and add new Task

---
+++ src/Evacuation.Infrastructure/Repositories/GenericWithIncludeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using Evacuation.Domain.Entities;
+using Evacuation.Infrastructure.Data.AppDbContext;
+using Evacuation.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evacuation.Infrastructure.Repositories
+{
+    public class GenericWithIncludeRepository<T, TKey>
+            : GenericRepository<T, TKey>, IGenericIncludeRepository<T, TKey>
+            where T : BaseEntityWithPrefix
+            where TKey : notnull
+    {
+        public GenericWithIncludeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<T>> GetAllWithInclude(params Expression<Func<T, object>>[] includes)
+        {
+            var query = GetQuery();
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdWithInclude(TKey key, params Expression<Func<T, object>>[] includes)
+        {
+            var query = GetQuery();
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(q => q.Id.Equals(key));
+        }
+
+        public Task<Dictionary<TKey, string>> GetIdMapAsync()
+        {
+            return _dbSet.AsNoTracking()
+            .ToDictionaryAsync(e => (TKey)(object)e.Id, e => e.BusinessId);
+        }
+    }
+}
---
+++ src/Evacuation.Infrastructure/Repositories/Interfaces/IGenericIncludeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Evacuation.Domain.Entities;
+
+namespace Evacuation.Infrastructure.Repositories.Interfaces
+{
+    public interface IGenericIncludeRepository<T, TKey> : IGenericRepository<T, TKey>
+            where T : BaseEntityWithPrefix
+            where TKey : notnull
+    {
+        Task<Dictionary<TKey, string>> GetIdMapAsync();
+        Task<IEnumerable<T>> GetAllWithInclude(params Expression<Func<T, object>>[] includes);
+        Task<T?> GetByIdWithInclude(TKey key, params Expression<Func<T, object>>[] includes);
+    }
+}
