a53c84481ab2f72ad1f122e3588a2ac16cb2cdd9
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-11-07 13:50:08 +0700

    Add : V1 and V2 controllers for Vehicles and Zones with CRUD operations

---
+++ src/Evacuation.API/Controllers/V2/ZonesController.cs
@@ -0,0 +1,117 @@
+﻿using Evacuation.Application.DTOs.Zone;
+using Evacuation.Application.Services.Factory.Interfaces;
+using Evacuation.Application.Services.Interfaces;
+using Evacuation.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Evacuation.API.Controllers.V2
+{
+    [Route("api/v2/zones")]
+    [ApiController]
+    [Authorize(Roles = "ADMIN,MANAGER")]
+    public class ZonesController : ControllerBase
+    {
+        private readonly IZoneService _zoneService;
+        public ZonesController(IServiceFactory serviceFactory)
+        {
+            _zoneService = serviceFactory.CreateZoneService(DatabaseType.Postgres);
+        }
+        // GET: api/<ZoneController>
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var result = await _zoneService.GetAllZonesAsync();
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving zones", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // GET api/<ZoneController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var result = await _zoneService.GetZoneByIdAsync(id);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while retrieving the zone", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // POST api/<ZoneController>
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromBody] CreateZoneDto zoneDto)
+        {
+            var result = await _zoneService.AddZoneAsync(zoneDto);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while adding the zone", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.Message });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // PUT api/<ZoneController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] UpdateZoneDto zoneDto)
+        {
+            var result = await _zoneService.UpdateZoneAsync(id, zoneDto);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while updating the zone", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.Message, Errors = result.Errors });
+                }
+            }
+            return Ok(result.Data);
+        }
+
+        // DELETE api/<ZoneController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _zoneService.DeleteZoneAsync(id);
+            if (!result.IsSuccess)
+            {
+                if (result.Exception != null)
+                {
+                    return StatusCode(500, new { Message = "An error occurred while deleting the zone", Error = result.Exception.Message });
+                }
+                else
+                {
+                    return NotFound(new { Message = result.Message });
+                }
+            }
+            return Ok(new { Message = $"Zone with ID {id} deleted successfully" });
+        }
+    }
+}
--- src/Evacuation.API/Controllers/VehiclesController.cs
+++ src/Evacuation.API/Controllers/VehiclesController.cs
@@ -1,114 +1,114 @@
-﻿using Evacuation.Application.DTOs.Vehicle;
-using Evacuation.Application.Services.Interfaces;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿// using Evacuation.Application.DTOs.Vehicle;
+// using Evacuation.Application.Services.Interfaces;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Evacuation.API.Controllers
-{
-    [Route("api/vehicles")]
-    [ApiController]
-    [Authorize(Roles = "ADMIN,MANAGER")]
-    public class VehiclesController : ControllerBase
-    {
-        private readonly IVehicleService _vehicleService;
-        public VehiclesController(IVehicleService vehicleService)
-        {
-            _vehicleService = vehicleService;
-        }
-        // GET: api/<VehicleController>
-        [HttpGet]
-        public async Task<IActionResult> GetAllAsync()
-        {
-            var result = await _vehicleService.GetAllVehiclesAsync();
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred while retrieving vehicles", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return NotFound(new { Message = result.Message });
-                }
-            }
-            return Ok(result.Data);
-        }
+// namespace Evacuation.API.Controllers
+// {
+//     [Route("api/vehicles")]
+//     // [ApiController]
+//     [Authorize(Roles = "ADMIN,MANAGER")]
+//     public class VehiclesController : ControllerBase
+//     {
+//         private readonly IVehicleService _vehicleService;
+//         public VehiclesController(IVehicleService vehicleService)
+//         {
+//             _vehicleService = vehicleService;
+//         }
+//         // GET: api/<VehicleController>
+//         [HttpGet]
+//         public async Task<IActionResult> GetAllAsync()
+//         {
+//             var result = await _vehicleService.GetAllVehiclesAsync();
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred while retrieving vehicles", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return NotFound(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
 
-        // GET api/<VehicleController>/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetByIdAsync(int id)
-        {
-            var result = await _vehicleService.GetVehicleByIdAsync(id);
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred while retrieving the vehicle", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return NotFound(new { Message = result.Message });
-                }
-            }
-            return Ok(result.Data);
-        }
+//         // GET api/<VehicleController>/5
+//         [HttpGet("{id}")]
+//         public async Task<IActionResult> GetByIdAsync(int id)
+//         {
+//             var result = await _vehicleService.GetVehicleByIdAsync(id);
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred while retrieving the vehicle", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return NotFound(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
 
-        // POST api/<VehicleController>
-        [HttpPost]
-        public async Task<IActionResult> AddAsync([FromBody] CreateVehicleDto vehicleDto)
-        {
-            var result = await _vehicleService.AddVehicleAsync(vehicleDto);
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred while adding the vehicle", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return BadRequest(new { Message = result.Message });
-                }
-            }
-            return Ok(result.Data);
-        }
+//         // POST api/<VehicleController>
+//         [HttpPost]
+//         public async Task<IActionResult> AddAsync([FromBody] CreateVehicleDto vehicleDto)
+//         {
+//             var result = await _vehicleService.AddVehicleAsync(vehicleDto);
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred while adding the vehicle", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return BadRequest(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
 
-        // PUT api/<VehicleController>/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAsync(int id, [FromBody] UpdateVehicleDto vehicleDto)
-        {
-            var result = await _vehicleService.UpdateVehicleAsync(id, vehicleDto);
-            if (!result.IsSuccess) 
-            {
-                if (result.Exception != null) 
-                {
-                    return StatusCode(500, new { Message = "An error occurred while updating the vehicle", Error = result.Exception.Message });
-                }
-                else {
-                    return BadRequest(new { Message = result.Message });
-                }
-            }
-            return Ok(result.Data);
-        }
+//         // PUT api/<VehicleController>/5
+//         [HttpPut("{id}")]
+//         public async Task<IActionResult> UpdateAsync(int id, [FromBody] UpdateVehicleDto vehicleDto)
+//         {
+//             var result = await _vehicleService.UpdateVehicleAsync(id, vehicleDto);
+//             if (!result.IsSuccess) 
+//             {
+//                 if (result.Exception != null) 
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred while updating the vehicle", Error = result.Exception.Message });
+//                 }
+//                 else {
+//                     return BadRequest(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
 
-        // DELETE api/<VehicleController>/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            var result = await _vehicleService.DeleteVehicleAsync(id);
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred while deleting the vehicle", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return NotFound(new { Message = result.Message });
-                }
-            }
-            return Ok(new { Message = $"Vehicle with ID {id} deleted successfully" });
-        }
-    }
-}
+//         // DELETE api/<VehicleController>/5
+//         [HttpDelete("{id}")]
+//         public async Task<IActionResult> DeleteAsync(int id)
+//         {
+//             var result = await _vehicleService.DeleteVehicleAsync(id);
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred while deleting the vehicle", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return NotFound(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(new { Message = $"Vehicle with ID {id} deleted successfully" });
+//         }
+//     }
+// }
