3acb7e409df902264fe64e0d9cc34e826e899be1
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2025-12-06 21:24:55 +0700

    Refactor : Comment out unused code in Auth, Evacuations, and Users controllers

--- src/Evacuation.API/Controllers/AuthController.cs
+++ src/Evacuation.API/Controllers/AuthController.cs
@@ -1,58 +1,59 @@
-﻿using Evacuation.Application.DTOs.User;
-using Evacuation.Application.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.RateLimiting;
+﻿// using Evacuation.Application.DTOs.User;
+// using Evacuation.Application.Services.Interfaces;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.RateLimiting;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Evacuation.API.Controllers
-{
-    [Route("api/auth")]
-    [ApiController]
-    public class AuthController : ControllerBase
-    {
-        private readonly IUserService _userService;
-        public AuthController(IUserService userService)
-        {
-            _userService = userService;
-        }
+// namespace Evacuation.API.Controllers
+// {
+//     [ApiVersion("2")]
+//     [Route("api/v{version:apiVersion}/auth")]
+//     [ApiController]
+//     public class AuthController : ControllerBase
+//     {
+//         private readonly IUserService _userService;
+//         public AuthController(IUserService userService)
+//         {
+//             _userService = userService;
+//         }
 
-        // POST api/<AuthController>/login
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
-        {
-            var result = await _userService.LoginAsync(loginDto);
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred during login", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return Unauthorized(new { Message = result.Message });
-                }
-            }
-            return Ok(result.Data);
-        }
+//         // POST api/<AuthController>/login
+//         [HttpPost("login")]
+//         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+//         {
+//             var result = await _userService.LoginAsync(loginDto);
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred during login", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return Unauthorized(new { Message = result.Message });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
 
-        // POST api/<AuthController>/register
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterUserDto registerDto)
-        {
-            var result = await _userService.RegisterUserAsync(registerDto);
-            if (!result.IsSuccess)
-            {
-                if (result.Exception != null)
-                {
-                    return StatusCode(500, new { Message = "An error occurred during registration", Error = result.Exception.Message });
-                }
-                else
-                {
-                    return BadRequest(new { Message = result.Message, Errors = result.Errors });
-                }
-            }
-            return Ok(result.Data);
-        }
-    }
-}
+//         // POST api/<AuthController>/register
+//         [HttpPost("register")]
+//         public async Task<IActionResult> Register([FromBody] RegisterUserDto registerDto)
+//         {
+//             var result = await _userService.RegisterUserAsync(registerDto);
+//             if (!result.IsSuccess)
+//             {
+//                 if (result.Exception != null)
+//                 {
+//                     return StatusCode(500, new { Message = "An error occurred during registration", Error = result.Exception.Message });
+//                 }
+//                 else
+//                 {
+//                     return BadRequest(new { Message = result.Message, Errors = result.Errors });
+//                 }
+//             }
+//             return Ok(result.Data);
+//         }
+//     }
+// }
